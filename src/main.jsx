7de7b54569b0ae3b95620d006ab85b805a54fd67
chuck445coder <pgkhiem040405.nvtroi2023@gmail.com>
2024-08-13 14:13:58 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/youtube-clone/'>
     <App />
   </BrowserRouter>
 )
